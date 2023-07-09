7657661d8fe61676d6b584d5154c365388f0b290
darketomaly@gmail.com <darketomaly@gmail.com>
2023-07-09 03:05:18 -0600

    - Removed singleton manager as it wasn't working as expected - Display camera is part of the fps-player singleton. Reason it wasn't before it was because the root transform was rotated by the rig

--- Assets/Behaviors/FPS Player/Display Camera/DisplayCamera.cs
+++ Assets/Behaviors/FPS Player/Display Camera/DisplayCamera.cs
@@ -6,39 +6,23 @@ namespace DarkFramework
     public class DisplayCamera : MonoBehaviour
     {
         public Canvas m_Canvas;
+        public FPSPlayer m_FpsPlayer;
     
-        public Transform m_LeftEyeAnchor;
-        public Transform m_RightEyeAnchor;
-        public Transform m_CenterEyeAnchor;
-
-        [InspectorButton(nameof(Center))]
         public Transform m_Transform;
-
-        private void Awake()
-        {
-            m_Transform = transform;
-            m_Canvas.gameObject.SetActive(true);
-        }
 
         private void Start()
         {
-            m_Transform.parent = null;
-        }
-
-        private void Center()
-        {
-            transform.position = m_CenterEyeAnchor.position;
-            transform.rotation = m_CenterEyeAnchor.rotation;
+            m_Canvas.gameObject.SetActive(true);
         }
 
         private void Update()
         {
-            m_Transform.position = m_CenterEyeAnchor.position;
+            m_Transform.position = m_FpsPlayer.m_CenterEyeAnchor.position;
         }
 
         void FixedUpdate()
         {
-            m_Transform.rotation = Quaternion.Lerp(m_Transform.rotation, m_CenterEyeAnchor.rotation, 5.0f * Time.deltaTime);
+            m_Transform.rotation = Quaternion.Lerp(m_Transform.rotation, m_FpsPlayer.m_CenterEyeAnchor.rotation, 5.0f * Time.deltaTime);
         }
     }
 }
--- Assets/Behaviors/FPS Player/Locomotion/FPSPlayer.cs
+++ Assets/Behaviors/FPS Player/Locomotion/FPSPlayer.cs
@@ -1,16 +1,45 @@
-using System;
 using UnityEngine;
 
-/// <summary>
-/// Contains the VR rig and player controller
-/// </summary>
-public class FPSPlayer : SingletonMonoBehaviour
+namespace DarkFramework
 {
-    private void Start()
+    /// <summary>
+    /// Contains the VR rig and player controller
+    /// </summary>
+    public class FPSPlayer : MonoBehaviour
     {
-        if (m_Initialized)
+        public static FPSPlayer Instance;
+        
+        public Transform m_CenterEyeAnchor;
+        
+        public bool m_Initialized;
+
+        protected void Awake()
         {
-            transform.GetChild(0).gameObject.SetActive(true);
+            if (Instance)
+            {
+                if (Instance != this)
+                {
+                    name += " (Bad clone)";
+                    Debug.LogError($"<color=olive>Destroying {name} because singleton already exists</color>");
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                m_Initialized = true;
+                Instance = this;
+                name += " (Good clone)";
+                transform.parent = null;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            if (m_Initialized)
+            {
+                transform.GetChild(0).gameObject.SetActive(true);
+            }
         }
     }
 }
